e3d085cda1eb7b2eb3606d86f3037849006fa1c0
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-01-21 03:56:41 -0500

    fix: styled header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./index.css";
-
+import Header from "./components/Header";
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
