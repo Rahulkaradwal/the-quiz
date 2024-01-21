a542da05bef68c6776d34562f533277401da0b77
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-01-21 04:13:01 -0500

    feat: fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import "./index.css";
 import Header from "./components/Header";
+import { useEffect, useReducer } from "react";
+
+const initialData = {
+  questions: [],
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondRemaining: null,
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    default:
+      throw new Error();
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialData);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
